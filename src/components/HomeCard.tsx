a9294a4d01a6070f80ab0e58ed8ad5d048c57c31
victormhp <victoralbertomhp@gmail.com>
2023-05-30 15:09:54 -0600

    Update HomeCard Component

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -5,9 +5,17 @@ function HomeCard() {
 	return (
 		<Card className='w-full flex flex-col gap-6'>
 			<Title>Github Offers</Title>
-			<div className='flex flex-row justify-around sm:flex-col'>
-				<Subtitle>Encuentra las ofertas que mejor se adapten a tu perfil como programdor</Subtitle>
-				<Image width={100} height={100} src='/pic-search.svg' alt='Search' />
+			<div className='flex flex-col justify-center items-center'>
+				<Subtitle>
+					¡Bienvenido(a) a nuestro filtrador de ofertas de trabajo de InfoJobs personalizado para usuarios de GitHub! Para comenzar, ingresa
+					tu nombre de usuario de GitHub en la barra de búsqueda y verás las ofertas de trabajo filtradas según los lenguajes más utilizados
+					en tus repositorios.
+				</Subtitle>
+				<picture>
+					<source media='(min-width: 640px)' srcSet='/content.svg' />
+					<source media='(max-width: 640px)' srcSet='/pic-search.svg' />
+					<img src='/pic-search.svg' alt='Search' />
+				</picture>
 			</div>
 		</Card>
 	);
