085ed7a37aadff2f777e9acbbb296baf70bb41d0
victormhp <victoralbertomhp@gmail.com>
2023-05-30 12:43:58 -0600

    Update card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,9 @@
 function Card() {
-	return <div>Card</div>;
+	return (
+		<div className='bg-white rounded p-5'>
+			<h3>Descubre las ofertas que mejor se adapten a tu perfil de Github.</h3>
+		</div>
+	);
 }
 
 export default Card;
